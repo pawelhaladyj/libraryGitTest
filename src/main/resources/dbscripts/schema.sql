73f425433ee5f3ed80d6c3e8af8c95086503617f
Miroslaw Mikula <info@softmed.pl>
2019-02-28 21:47:55 +0100

    modify schema.sql

--- src/main/resources/dbscripts/schema.sql
+++ src/main/resources/dbscripts/schema.sql
@@ -4,8 +4,9 @@
 
 CREATE TABLE person(
   id bigint(20) NOT NULL AUTO_INCREMENT,
-  name varchar(20) NOT NULL,
+  name varchar(25) NOT NULL,
   code_name varchar(9) NOT NULL UNIQUE,
   description varchar(255) DEFAULT NULL,
+  age int(3) DEFAULT NULL,
   PRIMARY KEY (id)
 )ENGINE=MyISAM DEFAULT CHARSET=utf8;
